c8a1762b95934e706abb63c1b0037879a70fb3d5
Semmelon <138894594+Semmelon@users.noreply.github.com>
2025-05-31 14:36:32 +0200

    use case 2 get customer balance by svn

--- DBI/Projekt_2025/functions_procedures.sql
+++ DBI/Projekt_2025/functions_procedures.sql
@@ -1,38 +1,44 @@
 -- customer package
 CREATE OR REPLACE PACKAGE customer_api IS
     PROCEDURE register_customer(
-        p_svz IN VARCHAR2,
-        p_firstname IN VARCHAR2,
-        p_lastname IN VARCHAR2,
-        p_birthdate IN DATE
+        svz IN VARCHAR2,
+        firstname IN VARCHAR2,
+        lastname IN VARCHAR2,
+        birthdate IN DATE
     );
+
+    FUNCTION get_customer_balance(
+        svz IN VARCHAR2
+    ) RETURN NUMBER;
 END customer_api;
 
 -- customer package body
 CREATE OR REPLACE PACKAGE BODY customer_api IS
 
+    -- private helper function for register_customer
     FUNCTION get_default_money RETURN NUMBER IS
     BEGIN
         RETURN 100.00;
     END;
 
+    -- private helper function for register_customer
     FUNCTION validate_age(p_birthdate DATE) RETURN BOOLEAN IS
     BEGIN
         RETURN (MONTHS_BETWEEN(SYSDATE, p_birthdate) / 12) >= 18;
     END;
 
-    -- Public: Register a new customer
+    -- public procedure
     PROCEDURE register_customer(
-        p_svz IN VARCHAR2,
-        p_firstname IN VARCHAR2,
-        p_lastname IN VARCHAR2,
-        p_birthdate IN DATE
+        svz IN VARCHAR2,
+        firstname IN VARCHAR2,
+        lastname IN VARCHAR2,
+        birthdate IN DATE
     ) IS
         v_new_id customer.id%TYPE;
         v_money  customer.money%TYPE := get_default_money;
     BEGIN
         -- Age check
-        IF NOT validate_age(p_birthdate) THEN
+        IF NOT validate_age(birthdate) THEN
             RAISE_APPLICATION_ERROR(-20002, 'Customer must be at least 18 years old.');
         END IF;
 
@@ -41,7 +47,38 @@ CREATE OR REPLACE PACKAGE BODY customer_api IS
 
         -- Insert new customer with default money
         INSERT INTO customer(id, svz, firstname, lastname, birthdate, money)
-        VALUES (v_new_id, p_svz, p_firstname, p_lastname, p_birthdate, v_money);
+        VALUES (v_new_id, svz, firstname, lastname, birthdate, v_money);
+    END;
+
+    -- private helper function for get_customer_balance
+    FUNCTION get_customer_id_by_svz(s_svz VARCHAR2) RETURN customer.id%TYPE IS
+        id customer.id%TYPE;
+    BEGIN
+        SELECT id
+        INTO id
+        FROM customer
+        WHERE svz = s_svz;
+
+        RETURN id;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20003, 'Customer with given SVZ does not exist.');
+    END;
+
+    -- public function
+    FUNCTION get_customer_balance(
+        svz IN VARCHAR2
+    ) RETURN NUMBER IS
+        balance customer.money%TYPE;
+        i_id      customer.id%TYPE;
+    BEGIN
+        i_id := get_customer_id_by_svz(svz);
+        SELECT money
+        INTO balance
+        FROM customer
+        WHERE id = i_id;
+
+        RETURN balance;
     END;
 
 END customer_api;
@@ -50,10 +87,18 @@ END customer_api;
 
 -- test for register customer
 BEGIN
-  customer_api.register_customer(
-    p_svz       => '987643210',
-    p_firstname => 'Alice',
-    p_lastname  => 'Smith',
-    p_birthdate => TO_DATE('2290-07-01', 'YYYY-MM-DD')
-  );
+    customer_api.register_customer(
+            svz => '987643210',
+            firstname => 'Alice',
+            lastname => 'Smith',
+            birthdate => TO_DATE('2290-07-01', 'YYYY-MM-DD')
+    );
+END;
+
+-- test for get customer balance
+DECLARE
+  v_balance NUMBER;
+BEGIN
+  v_balance := customer_api.get_customer_balance('9876543210');
+  DBMS_OUTPUT.PUT_LINE('Customer Balance: ' || v_balance);
 END;
