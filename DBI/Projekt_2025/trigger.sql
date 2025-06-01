db45f51f4614258b29c2a92f6236dd7081623cdd
Semmelon <138894594+Semmelon@users.noreply.github.com>
2025-06-01 10:31:50 +0200

    make trigger to auto update customers balance after game_session

--- DBI/Projekt_2025/trigger.sql
+++ DBI/Projekt_2025/trigger.sql
@@ -1,3 +1,4 @@
+-- chack taht customer cannot be removed if balance is negative
 CREATE OR REPLACE TRIGGER prevent_delete_negative_money
 BEFORE DELETE ON customer
 FOR EACH ROW
@@ -5,4 +6,16 @@ BEGIN
   IF :OLD.money < 0 THEN
     RAISE_APPLICATION_ERROR(-20001, 'Cannot delete customer with negative money balance.');
   END IF;
+END;
+
+-- update a cusomers balance after he played a session and the entry gets added in game_history
+
+CREATE OR REPLACE TRIGGER trg_update_customer_balance
+AFTER INSERT ON game_history
+FOR EACH ROW
+BEGIN
+  -- update cusomer
+  UPDATE customer
+  SET money = money + :NEW.payout
+  WHERE id = :NEW.customer;
 END;
